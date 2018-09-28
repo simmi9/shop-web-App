aa48fbcddfa00e0dae776b8628f883180dc1bf3e
simmi9 <bhawna.keswani20@gmail.com>
2018-09-28 01:11:07 -0400

    Created HttpService class to fetch result from server and logging in json format

--- shop-web/src/services/http-service.js
+++ shop-web/src/services/http-service.js
@@ -1,1 +1,10 @@
 import 'whatwg-fetch';
+class HttpService
+    { 
+        getProducts = () => { 
+        	 fetch('http://localhost:3007/product').then(response => {console.log(response.json());
+                                                                     })
+                            }
+    }      
+export default HttpService;
+                                
