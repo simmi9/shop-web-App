44f7ef9a1ce27e1be0c0f54a0b048fe49fb92554
simmi9 <bhawna.keswani20@gmail.com>
2018-09-28 01:55:14 -0400

    Creating  Promise variable to acknowlege server that you have received data

--- shop-web/src/services/http-service.js
+++ shop-web/src/services/http-service.js
@@ -1,10 +1,28 @@
 import 'whatwg-fetch';
-class HttpService
+/*class HttpService
     { 
         getProducts = () => { 
         	 fetch('http://localhost:3007/product').then(response => {console.log(response.json());
                                                                      })
                             }
-    }      
+    } 
+
+*/
+class HttpService
+    { 
+        getProducts =()=>{ var promise =new Promise((resolve, reject)=>{
+                         fetch('http://localhost:3007/product').then(response=>{
+                             resolve(response.json());
+                             //response.header("Access-Control-Allow-Origin", "*");
+                                                        })
+        });
+            
+         return promise;
+                         
+        
+                        }
+            
+    }
+
+
 export default HttpService;
-                                
